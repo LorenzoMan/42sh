30afb2c64c2dd7dbd164c185ab5ec8e52e721269
LorenzoMan <56729229+LorenzoMan@users.noreply.github.com>
2020-07-06 13:26:58 +0200

    Add files via upload

---
+++ lib/my/my_str_isnum.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2019
+** my_str_isnum
+** File description:
+** my_str_isnum
+*/
+
+int is_char_num(char c)
+{
+    return (c >= '0' && c <= '9');
+}
+
+int    my_str_isnum(char const *str)
+{
+    for (int current = 0; str[current]; current += 1) {
+        if (current == 0 && str[current] == '-')
+            continue;
+        if (!is_char_num(str[current]))
+            return (0);
+    }
+    return (1);
+}
